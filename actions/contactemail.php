2afd4483e258c8b04a4dd692668d1f46fb112f9f
deepthi <draghun1@terpmail.umd.edu>
2017-09-10 22:23:05 -0400

    adding php send email script

--- actions/contactemail.php
+++ actions/contactemail.php
@@ -1,1 +1,15 @@
-<?php echo '<p>Hello World</p>'; ?> 
+<?php 
+    $to = "westminstermmg@gmail.com"; 
+    $from = $_POST['Email'];
+    $name = $_POST['Name'];
+    $subject = "Contact";
+    $subject2 = "Copy of your form submission";
+    $message = $name  . " wrote the following:" . "\n\n" . $_POST['Message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['Message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); 
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+?>
